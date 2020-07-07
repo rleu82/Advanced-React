6ad34a21726fd0dc344d92849a89dd9ddc5d4d98
Robert Leu <robertleu@gmail.com>
2020-07-07 14:30:10 -0700

    working on schema

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,8 @@
-const mutations = {};
+const Mutations = {
+    createDog(parent, args, ctx, info) {
+        //create dog
+        console.log(args);
+    },
+};
 
-module.exports = mutations;
+module.exports = Mutations;
